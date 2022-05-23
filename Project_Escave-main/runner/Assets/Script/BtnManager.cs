710386a98b39b43818af55710e5aee61d1233445
ddkaise <usuk333@gmail.com>
2022-05-23 17:05:54 +0900

    753
    753

---
+++ Project_Escave-main/runner/Assets/Script/BtnManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject GameOver;
+
+    MapMove gmst;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MapMove gmst = GameObject.Find("Map").GetComponent<MapMove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Restart()
+    {
+        GameOver.SetActive(false);
+        Obstacle.gameOver = false ;
+        gmst.GameStart();
+        Time.timeScale = 1f;
+    }
+}
